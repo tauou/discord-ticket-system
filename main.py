dc3ab17ea21db2d99f5a46d6ca2217896fd589cd
tauou <73067662+tauou@users.noreply.github.com>
2021-10-12 12:37:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,7 @@ from discord.ext.commands import has_permissions, MissingPermissions
 import json
 import asyncio
 
-bot = commands.Bot(command_prefix=".")
-bot.remove_command("help")
 
-@bot.event
-async def on_ready():
-    print("Bot running with:")
-    print("Username: ", bot.user.name)
-    print("User ID: ", bot.user.id)
 
 @bot.command()
 async def help(ctx):
